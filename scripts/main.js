ebb432ab7aca7e089c58ecdc42e3905f3fad4e64
guiparpineli <guiparpineli@hotmail.com>
2022-04-23 20:34:03 -0300

    clean code

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,6 @@
 const selectId = a => document.getElementById(a);
 
-const clean = selectId('clean');
-const seven = selectId(7);
-const eight = selectId(8);
-const nine = selectId(9);
-const mult = selectId('mult');
-const four = selectId(4);
-const five = selectId(5);
-const six = selectId(6);
-const sub = selectId('subtracao');
-const one = selectId(1);
-const two = selectId(2);
-const three = selectId(3);
 const add = selectId('adicao');
-const porcent = selectId('porcent');
-const zero = selectId(0);
-const done = selectId('btn-equal');
-const division = selectId('division');
-const dot = selectId('dot');
-const hundred = selectId(00);
-
 
 var idElement = '';
 var numbers = [];
@@ -50,7 +31,7 @@ window.addEventListener("click", function (event) {
     }
 
     add.onclick = () => {
-        
+
         ptext = document.createTextNode('+')
         p.appendChild(ptext)
         p.classList.add('visor-text')
@@ -58,23 +39,18 @@ window.addEventListener("click", function (event) {
 
         var numberJoin = numbers.join('');
         var numberSoma = parseInt(numberJoin, 10);
-
-
-
         newNumber.push(numberSoma);
 
         var soma = () => {
             if (newNumber.length >= 2) {
                 screen.innerHTML = '';
                 var result = newNumber.reduce((a, b) => a + b);
-               
+
                 screen.innerHTML = `<p class='visor-text'> ${result} </p>`
                 console.log(result)
             }
         }
-
         numbers = [];
-
         soma();
     }
 }
