fa1592ed1f16e514b77da9e714c7cdf5f59aef12
guiparpineli <guiparpineli@hotmail.com>
2022-04-23 20:01:12 -0300

    logica inicial soma

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Calculator
+
+Projeto tem a ideia de sintetizar todo conhecimento que obtive, sem usar tutoriais, apenas minha lógica e documentação como da Mozila.
--- scripts/main.js
+++ scripts/main.js
@@ -20,13 +20,72 @@ const division = selectId('division');
 const dot = selectId('dot');
 const hundred = selectId(00);
 
-var target = '';
+
 var idElement = '';
+var numbers = [];
+var classElement = '';
+
+
+var newNumber = [];
 
 window.addEventListener("click", function (event) {
-    if (event.target.className === 'number') {
-        target = event.target;
-        idElement = target.id
+    const screen = document.querySelector('.visor')
+    idElement = event.target.id;
+    classElement = event.target.className;
+
+    if (idElement === 'clean') {
+        const screen = document.querySelector('.visor')
+        newNumber = [];
+        screen.innerHTML = ''
     }
-});
+
+
+    if (classElement === 'number') {
+        numbers.push(idElement);
+        //appends
+        const screen = document.querySelector('.visor')
+
+        const p = document.createElement('p');
+        const ptext = document.createTextNode(idElement);
+
+        p.appendChild(ptext);
+        p.classList.add('.visor-text')
+        screen.appendChild(p)
+    }
+
+    add.onclick = () => {
+        const p = document.createElement('p');
+        const ptext = document.createTextNode('+')
+        p.appendChild(ptext)
+        p.classList.add('.visor-text')
+        screen.appendChild(p)
+
+        var numberJoin = numbers.join('');
+        var numberSoma = parseInt(numberJoin, 10);
+
+
+
+        newNumber.push(numberSoma);
+
+        var soma = () => {
+            if (newNumber.length >= 2) {
+                screen.innerHTML = '';
+                var result = newNumber.reduce((a, b) => a + b);
+                const p = document.createElement('p');
+                const ptext = document.createTextNode(result);
+                p.appendChild(ptext);
+                p.classList.add('.visor-text')
+                screen.appendChild(p)
+                console.log(result)
+            }
+        }
+
+        numbers = [];
+
+        soma();
+    }
+}
+);
+
+
 
