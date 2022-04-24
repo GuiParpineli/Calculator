8a076b1dcc93a5ca4fd061aab2f7a04e2f6e3906
Gui Parpineli <guiparpineli@hotmail.com>
2022-04-24 11:03:20 -0300

    em andamanto, ainda nao funcional, soma e sub

--- scripts/main.js
+++ scripts/main.js
@@ -1,78 +1,92 @@
 const selectId = a => document.getElementById(a);
-
 const add = selectId('adicao');
 const equal = selectId('btn-equal');
-
+const sub = selectId('subtracao');
 var idElement = '';
 var numbers = [];
 var classElement = '';
-const screen = document.querySelector('.visor')
-
-
+const screenNumber = selectId('screen-numbers');
+const resultado = selectId('resultado');
+var arraySub = [];
 var arraySoma = [];
 
 window.addEventListener("click", function (event) {
-    const screen = document.querySelector('.visor')
+
     idElement = event.target.id;
     classElement = event.target.className;
-    var p = document.createElement('p');
-    var ptext = document.createTextNode(idElement);
 
     if (idElement === 'clean') {
-        const screen = document.querySelector('.visor')
         arraySoma = [];
+        arraySub = [];
         numbers = [];
-        screen.innerHTML = ''
+        screenNumber.innerHTML = '';
+        resultado.innerHTML = '';
     }
-
 
     if (classElement === 'number') {
         numbers.push(idElement);
         //appends
-
-        if (arraySoma.length < 2) {
-            screen.innerHTML += `<p class='visor-text'> ${idElement} </p>`
+        if ((arraySoma.length < 2) || (arraySoma.length < 2)) {
+            screenNumber.innerHTML += `<p class='visor-text'> ${idElement} </p>`
         }
-        if (arraySoma.length >= 2) {
-            const resultado = document.getElementById('resultado')
-            resultado.innerHTML = '+'
-            screen.innerHTML += `<p class='visor-text'>  ${idElement} </p>`
 
+        if (arraySoma.length > 2) {
+            resultado.innerHTML = '+'
+            screenNumber.innerHTML += `<p class='visor-text'>  ${idElement} </p>`
+        }
+
+        if (arraySub.length > 2) {
+            resultado.innerHTML = '-'
+            screenNumber.innerHTML += `<p class='visor-text'>  ${idElement} </p>`
         }
     }
-
-
 
     var soma = () => {
 
         if (arraySoma.length >= 2) {
-            screen.innerHTML = '';
-            var result = arraySoma.reduce((a, b) => a + b);
-
-            screen.innerHTML = `<p class='visor-text' id='resultado'> ${result} </p>`
+            screenNumber.innerHTML = '';
+            resultado.innerHTML = '';
+            let result = arraySoma.reduce((a, b) => a + b);
+            resultado.innerHTML = `<p class='visor-text' id='resultado'> ${result} </p>`
             console.log(result)
         }
     }
-    add.onclick = () => {
-        screen.innerHTML = `<p class='visor-text' id='resultado'> + </p>`;
 
-        var numberJoin = numbers.join('');
-        var numberSoma = parseInt(numberJoin, 10);
+    var subtract = () => {
+        if (arraySub.length >= 2) {
+            screenNumber.innerHTML = '';
+            let result = arraySub.reduce((a, b) => a - b);
+            resultado.innerHTML = `<p class='visor-text' > ${result} </p>`
+            console.log(result)
+        }
+    }
+
+    add.onclick = () => {
+        screenNumber.innerHTML = `<p class='visor-text'> + </p>`;
+        let numberJoin = numbers.join('');
+        let numberSoma = parseInt(numberJoin, 10);
         arraySoma.push(numberSoma);
         numbers = [];
-
         soma();
+    }
+
+    sub.onclick = () => {
+        screenNumber.innerHTML = `<p class='visor-text'> - </p>`;
+        let numberJoin = numbers.join('');
+        let numberSub = parseInt(numberJoin, 10);
+        arraySub.push(numberSub);
+        numbers = [];
+        subtract();
     }
 
     equal.onclick = () => {
 
         if (arraySoma.length > 0) {
-            screen.innerHTML = `<p class='visor-text' id='resultado'> + </p>`;
+            resultado.innerHTML = `<p class='visor-text'> + </p>`;
             var numberJoin = numbers.join('');
             var numberSoma = parseInt(numberJoin, 10);
             arraySoma.push(numberSoma);
             numbers = [0];
-
             soma();
         }
 
