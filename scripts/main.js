f649cd2c8e69fabed20dff9e63e157d5195fd69f
Gui Parpineli <guiparpineli@hotmail.com>
2022-04-26 12:53:59 -0300

    corrijido redundancias e acc. mult

--- scripts/main.js
+++ scripts/main.js
@@ -3,68 +3,61 @@ const selectId = a => document.getElementById(a);
 const add = selectId('+');
 const equal = selectId('btn-equal');
 const sub = selectId('-');
+const mult = selectId('x')
 var idElement = '';
 var numbers = [0];
 var classElement = '';
 const screenNumber = selectId('screen-numbers');
 const resultado = selectId('resultado');
 var contOpr = 0;
-
-var arrayNumbers = [];
+var newresult = 0;
 var result = 0;
 var contAdd = false;
 var contSub = false;
-
+var contMult = false;
 //funcoes matematicas
 var soma = () => {
-    arrayNumbers = [0];
 
-    if (contOpr == 0) {
+    if (contOpr === 0) {
 
-        var numberJoin = numbers.join('');
-        var numberParse = parseInt(numberJoin, 10);
-        arrayNumbers.push(numberParse);
-        result = arrayNumbers.reduce((a, b) => (b + a));
+        let numberJoin = numbers.join('');
+        let numberParse = parseInt(numberJoin, 10);
+        result = numberParse;
         numbers = [0];
-    };
+    }
     if (contOpr > 0) {
-        var numberJoin = numbers.join('');
-        var numberParse = parseInt(numberJoin, 10);
-        arrayNumbers.push(numberParse);
-        numbers = [0];
-        newresult = arrayNumbers.reduce((a, b) => b + a);
+        let numberJoin = numbers.join('');
+        let numberParse = parseInt(numberJoin, 10);
+        newresult = numberParse
         result = (newresult) + (result);
-
-    };
+        numbers = [0];
+    }
 
     screenNumber.innerHTML = ` `;
     resultado.innerHTML = `<p class='visor-text' id='resultado'> ${result} </p>`
-   
+
     contAdd = true;
     contSub = false;
+    contMult = false;
     contOpr += 1;
 
 }
 
 var subtract = () => {
 
-    arrayNumbers = [0];
+    if (contOpr === 0) {
+        let numberJoin = numbers.join('');
+        let numberParse = parseInt(numberJoin, 10);
 
-    if (contOpr == 0) {
-        var numberJoin = numbers.join('');
-        var numberParse = parseInt(numberJoin, 10);
-        arrayNumbers.push(numberParse);
-        result = arrayNumbers.reduce((a, b) => (b - a));
+        result = numberParse;
         numbers = [0];
     }
     if (contOpr > 0) {
-        var numberJoin = numbers.join('');
-        var numberParse = parseInt(numberJoin, 10);
-        arrayNumbers.push(numberParse);
-
-        numbers = [0];
-        newresult = arrayNumbers.reduce((a, b) => b - a);
+        let numberJoin = numbers.join('');
+        let numberParse = parseInt(numberJoin, 10);
+        newresult = numberParse
         result = (-newresult) - (-result);
+        numbers = [0];
     }
 
     screenNumber.innerHTML = ` `;
@@ -73,8 +66,35 @@ var subtract = () => {
 
     contSub = true;
     contAdd = false;
+    contMult = false;
     contOpr += 1;
 
+}
+var multiply = () => {
+
+    if (contOpr === 0) {
+        let numberJoin = numbers.join('');
+        let numberParse = parseInt(numberJoin, 10);
+
+        result = numberParse;
+        numbers = [0];
+    }
+    if (contOpr > 0) {
+        let numberJoin = numbers.join('');
+        let numberParse = parseInt(numberJoin, 10);
+        newresult = numberParse
+        result = (newresult) * (result);
+        numbers = [0];
+    }
+
+    screenNumber.innerHTML = ` `;
+
+    resultado.innerHTML = `<p class='visor-text' id='resultado'> ${result} </p>`
+
+    contMult = true;
+    contSub = false;
+    contAdd = false;
+    contOpr += 1;
 }
 
 //eventos de click
@@ -89,21 +109,22 @@ window.addEventListener("click", function (event) {
         numbers = [];
         screenNumber.innerHTML = '';
         resultado.innerHTML = '';
-    };
+    }
 
     if (classElement === 'number') {
         numbers.push(idElement);
         screenNumber.innerHTML += `<p class='visor-text'>  ${idElement} </p>`
-    }
-
-    else {
+    } else {
         if (idElement === '+') {
             screenNumber.innerHTML += `<p class='visor-text'>  ${idElement} </p>`
         }
         if (idElement === '-') {
             screenNumber.innerHTML = `<p class='visor-text'>  ${idElement} </p>`
         }
-    };
+        if (idElement === 'x') {
+            screenNumber.innerHTML = `<p class='visor-text'>  ${idElement} </p>`
+        }
+    }
 
     add.onclick = () => {
         soma()
@@ -113,14 +134,20 @@ window.addEventListener("click", function (event) {
         subtract();
     };
 
+    mult.onclick = () => {
+        multiply();
+    }
     equal.onclick = () => {
 
         if (contAdd === true) {
             soma()
-        };
+        }
         if (contSub === true) {
             subtract()
-        };
+        }
+        if (contMult === true) {
+            multiply();
+        }
     };
 });
 
